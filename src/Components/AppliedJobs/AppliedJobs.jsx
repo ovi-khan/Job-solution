57fb031201b09ebc9358a685030f9cfb20d31f5e
ovi-khan <manikkhan2240@gmail.com>
2023-04-12 03:56:49 +0600

    filter btn create

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import ApplyJob from '../ApplyJobs/ApplyJob';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import ApplyJob from "../ApplyJobs/ApplyJob";
 
 const AppliedJobs = () => {
-    const { initialCart } = useLoaderData()
-    console.log(initialCart)
-    // const {id} = jobData.products
-    return (
-        <div className='mt-36'>
-            {
-                initialCart.map(cartData => <ApplyJob
-                    key={cartData.id}
-                    cartData={cartData}
-                ></ApplyJob>
-                    )
-            }
-        </div>
-    );
+  const { initialCart } = useLoaderData();
+  console.log(initialCart);
+  // const {id} = jobData.products
+  return (
+    <div className="mt-36">
+      <div className=" mx-[80%] border border-blue-500 mb-5 px-3 py-2">
+        <select className="" name="appliedJobs" id="appliedJobs">
+          <option value="volvo">Remote</option>
+          <option value="saab">Full Time</option>
+        </select>
+      </div>
+      <div className="">
+        {initialCart.map((cartData) => (
+          <ApplyJob key={cartData.id} cartData={cartData}></ApplyJob>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AppliedJobs;
--- src/Components/ApplyJobs/ApplyJob.jsx
+++ src/Components/ApplyJobs/ApplyJob.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ApplyJob = ({cartData}) => {
-    const {id, categoryLogo, jobTitle, companyName, fullTimeOrPartTime, remoteOrOnsite, location, salary} = cartData
-    console.log(cartData)
-    return (
-            <div className='flex mx-40 mt-10 justify-between justify-center items-center border border-red-400 p-5 rounded-lg'>
-             <div className='flex gap-10'>
-             <img className='w-24 object-cover' src={categoryLogo} alt="" />
-            <div className=''>
-                <h2 className='text-2xl'>{jobTitle}</h2>
-                <p>{companyName}</p>
-                <div>
-                    <button>Remote</button>
-                    <button>Full Time</button>
-                </div>
-                <p>{location}, {salary}</p>
+const ApplyJob = ({ cartData }) => {
+  const {
+    id,
+    categoryLogo,
+    jobTitle,
+    companyName,
+    fullTimeOrPartTime,
+    remoteOrOnsite,
+    location,
+    salary,
+  } = cartData;
+  console.log(cartData);
+  return (
+    <div className="mx-40 mt-10">
+      <div className="flex justify-between justify-center items-center border border-red-400 p-5 rounded-lg">
+        <div className="flex gap-10">
+          <img className="w-24 object-cover" src={categoryLogo} alt="" />
+          <div className="">
+            <h2 className="text-2xl">{jobTitle}</h2>
+            <p>{companyName}</p>
+            <div>
+              <button>Remote</button>
+              <button>Full Time</button>
             </div>
-             </div>
-           <div>
-           <button className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-5 py-3 rounded-lg text-white font-bold'>View Details</button>
-           </div>
+            <p>
+              {location}, {salary}
+            </p>
+          </div>
         </div>
-    );
+        <div>
+          {/* <Link> */}
+          <Link
+            to="/appliedjobs"
+            className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-5 py-3 rounded-lg text-white font-bold"
+          >
+            View Details
+          </Link>
+          {/* </Link> */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ApplyJob;
